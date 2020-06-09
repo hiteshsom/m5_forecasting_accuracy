1658ce4fa1c2ab3fc668c66e7113528c086b1f13
hiteshsom <hiteshsom@gmail.com>
2020-06-09 11:18:21 +0800

    Updating setup.py

---
+++ src/feature_engineering.py
@@ -0,0 +1,34 @@
+# https://www.kaggle.com/ragnar123/very-fst-model
+def simple_fe_extra(data):
+    
+    # rolling demand features
+    data['lag_t56'] = data.groupby(['id'])['demand'].transform(lambda x: x.shift(56))
+    data['lag_t57'] = data.groupby(['id'])['demand'].transform(lambda x: x.shift(57))
+    data['lag_t58'] = data.groupby(['id'])['demand'].transform(lambda x: x.shift(58))
+    data['rolling_mean_t7'] = data.groupby(['id'])['demand'].transform(lambda x: x.shift(56).rolling(7).mean())
+    data['rolling_std_t7'] = data.groupby(['id'])['demand'].transform(lambda x: x.shift(56).rolling(7).std())
+    data['rolling_mean_t30'] = data.groupby(['id'])['demand'].transform(lambda x: x.shift(56).rolling(30).mean())
+    data['rolling_mean_t90'] = data.groupby(['id'])['demand'].transform(lambda x: x.shift(56).rolling(90).mean())
+    data['rolling_mean_t180'] = data.groupby(['id'])['demand'].transform(lambda x: x.shift(56).rolling(180).mean())
+    data['rolling_std_t30'] = data.groupby(['id'])['demand'].transform(lambda x: x.shift(56).rolling(30).std())
+    data['rolling_skew_t30'] = data.groupby(['id'])['demand'].transform(lambda x: x.shift(56).rolling(30).skew())
+    data['rolling_kurt_t30'] = data.groupby(['id'])['demand'].transform(lambda x: x.shift(56).rolling(30).kurt())
+    
+    
+    # price features
+    data['lag_price_t1'] = data.groupby(['id'])['sell_price'].transform(lambda x: x.shift(1))
+    data['price_change_t1'] = (data['lag_price_t1'] - data['sell_price']) / (data['lag_price_t1'])
+    data['rolling_price_max_t365'] = data.groupby(['id'])['sell_price'].transform(lambda x: x.shift(1).rolling(365).max())
+    data['price_change_t365'] = (data['rolling_price_max_t365'] - data['sell_price']) / (data['rolling_price_max_t365'])
+    data['rolling_price_std_t7'] = data.groupby(['id'])['sell_price'].transform(lambda x: x.rolling(7).std())
+    data['rolling_price_std_t30'] = data.groupby(['id'])['sell_price'].transform(lambda x: x.rolling(30).std())
+    data.drop(['rolling_price_max_t365', 'lag_price_t1'], inplace = True, axis = 1)
+    
+    # time features
+    data['date'] = pd.to_datetime(data['date'])
+    data['week'] = data['date'].dt.week
+    data['day'] = data['date'].dt.day
+    data['dayofweek'] = data['date'].dt.dayofweek
+    
+    
+    return data
---
+++ src/m5_splitter.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+class M5Split2:
+    
+    def __init__(self, n_splits, group_id, date_col, train_size, gap_size, val_size, step):
+        self.n_splits = n_splits + 1
+        self.group_id = group_id
+        self.date_col = date_col
+        self.train_size = train_size
+        self.gap_size = gap_size
+        self.val_size = val_size
+        self.step = step
+        
+        
+    def split(self, df):
+        df = df.sort_values(by=[self.date_col])
+        indexes = []
+        group_indexes = np.array(df.groupby(self.group_id, observed=True).apply(lambda x: x.index))
+        for split in range(self.n_splits, 0, -1):
+            val_idx = []
+            gap_idx = []
+            train_idx = []
+                
+            for idx_arr in group_indexes:
+                
+                if self.train_size + self.gap_size + self.val_size + self.step*split > len(idx_arr):
+                    print(f'Max Split reached')
+                    break
+                    
+                val_idx += list(idx_arr[-(self.val_size + self.step*(split-1)):len(idx_arr) - 1 - self.step*(split-1)])
+                gap_idx += list(idx_arr[-(self.gap_size + self.val_size + self.step*(split-1)):-(self.val_size + self.step*(split-1))])
+                train_idx += list(idx_arr[-(self.train_size + self.gap_size + self.val_size + self.step*(self.n_splits)):-(self.val_size + self.gap_size + self.step*(split-1))])
+                
+            yield train_idx, gap_idx, val_idx
